73f0bf7010e356ce6f1191dea21e8d14f744c11c
Dmytro Laikov <LuckyDnepr@gmail.com>
2024-08-01 18:14:34 +0300

    Fix course type

--- dist/Course/Course.d.ts
+++ dist/Course/Course.d.ts
@@ -1,7 +1,9 @@
 import { Base } from '../database';
 import { User } from '../User';
+import { Wordkit } from '../Wordkit';
 export type Course = Base & {
     title: string;
     owner: User;
     listeners?: User[];
+    wordkits: Wordkit[];
 };
