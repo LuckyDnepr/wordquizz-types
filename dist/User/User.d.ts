777def16de645dc58f4972eb8c958b725a9f7ecb
Dmytro Laikov <LuckyDnepr@gmail.com>
2024-08-02 15:03:26 +0300

    Fix User type

--- dist/User/User.d.ts
+++ dist/User/User.d.ts
@@ -1,10 +1,13 @@
 import { UserScope } from './UserScope';
 import { Base } from '../database';
 import { Profile } from '../Profile';
+import { Course } from '../Course';
 export type User = Base & {
     email: string;
     password?: string;
     token?: string;
     scope: UserScope;
     profile?: Profile;
+    ownedCourses?: Course[];
+    listenedCourses?: Course[];
 };
--- src/User/User.ts
+++ src/User/User.ts
@@ -1,12 +1,15 @@
-import { UserScope } from './UserScope';
+import { UserScope } from './UserScope'
 
-import { Base } from '../database';
-import { Profile } from '../Profile';
+import { Base } from '../database'
+import { Profile } from '../Profile'
+import { Course } from '../Course'
 
 export type User = Base & {
-  email: string;
-  password?: string;
-  token?: string;
-  scope: UserScope;
-  profile?: Profile;
-};
+  email: string
+  password?: string
+  token?: string
+  scope: UserScope
+  profile?: Profile
+  ownedCourses?: Course[]
+  listenedCourses?: Course[]
+}
